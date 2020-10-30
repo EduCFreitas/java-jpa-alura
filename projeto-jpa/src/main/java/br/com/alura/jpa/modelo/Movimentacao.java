7d1b3c13d0be71ab2e973539b55be0bf19cee9a0
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-10-30 09:43:54 -0300

    Aula 4: Relacionamento Muitos para Muitos

--- projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
+++ projeto-jpa/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
@@ -79,5 +79,13 @@ public class Movimentacao {
 	public void setConta(Conta conta) {
 		this.conta = conta;
 	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
 	
 }
