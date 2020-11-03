95af253f15344189087b512b23759011ce492f84
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-11-03 12:40:07 -0300

    Aula 2.5: Query dinâmica

--- projeto-jpa/src/main/java/br/com/alura/jpa/modelo/dao/MovimentacaoDAO.java
+++ projeto-jpa/src/main/java/br/com/alura/jpa/modelo/dao/MovimentacaoDAO.java
@@ -1,10 +1,15 @@
 package br.com.alura.jpa.modelo.dao;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import br.com.alura.jpa.modelo.MediaComData;
 import br.com.alura.jpa.modelo.Movimentacao;
@@ -17,19 +22,34 @@ public class MovimentacaoDAO {
 	}
 	
 	public List<Movimentacao> getMovimentacoesFiltradasPorData(Integer dia, Integer mes, Integer ano){
-		String sql = "select m from Movimentacao m";
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Movimentacao> query = builder.createQuery(Movimentacao.class);
+		
+		Root<Movimentacao> root = query.from(Movimentacao.class);
+		
+		List<Predicate> predicates = new ArrayList<Predicate>();
 		
 		if(dia != null) {
-			sql = sql + " where day(m.data) = :pDia";
+			//day(m.data)
+			Predicate predicate = builder.equal(builder.function("day", Integer.class, root.get("data")), dia);
+			predicates.add(predicate);
 		}
 		if(mes != null) {
-			sql = sql + " and month(m.data) - :pMes";
+			//month(m.data)
+			Predicate predicate = builder.equal(builder.function("month", Integer.class, root.get("data")), mes);
+			predicates.add(predicate);
 		}
 		if(ano != null) {
-			sql = sql + " and year(m.data) - :pAno";
+			//year(m.data)
+			Predicate predicate = builder.equal(builder.function("year", Integer.class, root.get("data")), ano);
+			predicates.add(predicate);
 		}
 		
-		return null;
+		query.where((Predicate[]) predicates.toArray(new Predicate[0])); 
+		
+		TypedQuery<Movimentacao> typedQuery = em.createQuery(query);
+		
+		return typedQuery.getResultList();
 	}
 
 	public List<MediaComData> getMediaDiariaDasMovimentacoes(){
