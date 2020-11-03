5547e275da5af359d2464e624e09717622386ed3
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-11-03 12:17:36 -0300

    Aula 2.5: Usando CriteriaQuery

--- projeto-jpa/src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
+++ projeto-jpa/src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
@@ -1,21 +1,42 @@
 package br.com.alura.jpa.testes;
 
+import java.math.BigDecimal;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
 
-import br.com.alura.jpa.modelo.dao.MovimentacaoDAO;
+import br.com.alura.jpa.modelo.Movimentacao;
 
 public class TestaSomaDasMovimentacoes {
 
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
 		EntityManager em = emf.createEntityManager();
+
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<BigDecimal> query = builder.createQuery(BigDecimal.class);
 		
-		MovimentacaoDAO dao = new MovimentacaoDAO(em);
+		//select m from Movimentacao m
+		Root<Movimentacao> root = query.from(Movimentacao.class);
 		
-		System.out.println("A soma das movimentações é: " + dao.getSomaDasMovimentacoes());
-		System.out.println("A média das movimentações é: " + dao.getMediaDasMovimentacoes());
+		//select sum(m.valor)
+		Expression<BigDecimal> sum = builder.sum(root.<BigDecimal>get("valor"));
+		query.select(sum);
+		
+		TypedQuery<BigDecimal> typedQuery = em.createQuery(query);
+		
+		System.out.println("A soma das movimentações é: " + typedQuery.getSingleResult());
+		
+//		MovimentacaoDAO dao = new MovimentacaoDAO(em);
+		
+//		System.out.println("A soma das movimentações é: " + dao.getSomaDasMovimentacoes());
+//		System.out.println("A média das movimentações é: " + dao.getMediaDasMovimentacoes());
 
 	}
 
